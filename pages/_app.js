43bc8d5e1982593f8709ae4af17b1689934a1e3a
Mohammad Nayef <>
2021-10-01 16:50:59 +0300

    commit
    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../assets/css/custom.css";
 import "react-datepicker/dist/react-datepicker.css";
+import HotelsContextProvider from "../src/contexts/HotelsContexts";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <HotelsContextProvider>
+      <Component {...pageProps} />
+    </HotelsContextProvider>
+  );
 }
 
 export default MyApp;

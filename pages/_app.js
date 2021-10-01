95ebb1d2f22ff2430d3e76d7444f0ad4da8c2986
Mohammad Nayef <>
2021-10-01 16:36:31 +0300

    commit
    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../assets/css/custom.css";
+import "react-datepicker/dist/react-datepicker.css";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;

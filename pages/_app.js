36c9f5a3fe484a3de4cd18cb37ca792290b23ffd
Mohammad Nayef <>
2021-10-02 09:20:39 +0300

    Commit
    Commit Changes

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,11 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../assets/css/custom.css";
 import "react-datepicker/dist/react-datepicker.css";
 import HotelsContextProvider from "../src/contexts/HotelsContexts";
+import Navbar from "../src/components/Navbar";
 function MyApp({ Component, pageProps }) {
   return (
     <HotelsContextProvider>
+      <Navbar />
       <div className="container app-container">
         <Component {...pageProps} />
       </div>
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="collapse navbar-collapse container">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link href="/">
+              <a className="nav-link">Home</a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/hotels">
+              <a className="nav-link">Hotels</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+export default Navbar;
